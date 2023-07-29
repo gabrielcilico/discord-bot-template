9dad3b0b5976ae0714461c96b090f8dfa66b9138
gabrielcilico <gabrielcilico@gmail.com>
2023-07-28 21:45:38 -0300

    add config

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  app: {
+    token: "XXX",
+    credits: "Made with ❤️ by cilico.dev",
+    global: true,
+    client: "",
+    guild: "700761715041632369",
+  },
+
+  colors: {
+    primary: "#8844ee",
+    secondary: "#EB459E",
+    success: "#57F287",
+    error: "#ED4245",
+    warn: "#FEE75C",
+    info: "#5865F2",
+  },
+
+  opt: {},
+};
